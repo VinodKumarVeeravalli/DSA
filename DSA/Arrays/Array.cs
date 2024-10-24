f8a6282aabb5cfc0e6eeedb0233b34faa379bd3d
vino74478 <vkumar74478@gmail.com>
2024-10-24 22:57:08 +0530

    Array indexOf, removeAt, isert implementation

---
+++ DSA/Arrays/Array.cs
@@ -0,0 +1,71 @@
+﻿namespace DSA.Arrays
+{
+    public class Array
+    {
+        private int[] items;
+        private int size;
+
+        public Array(int length)
+        {
+            items = new int[length];
+        }
+
+        public void Insert(int value)
+        {
+            if (items.Length == size)
+            {
+                int[] newItems = new int[size * 2];
+
+                for (int i = 0; i < size; i++)
+                {
+                    newItems[i] = items[i];
+                }
+                items = newItems;
+            }
+
+            items[size] = value;
+            size++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            for (int i = index; i < size; i++)
+            {
+                items[i] = items[i + 1];
+            }
+
+            size--;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(items[i]);
+            }
+        }
+
+        public int IndexOf(int value)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (items[i] == value)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static void Reverse(int[] arr)
+        {
+            //to do
+        }
+    }
+}
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,22 +1,22 @@
-﻿using DSA.Arrays;
+﻿//using DSA.Arrays;
 
-namespace DSA
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 45, 67 };
+//namespace DSA
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 45, 67 };
 
-            ArrayReverse arrObj = new ArrayReverse();
+//            ArrayReverse arrObj = new ArrayReverse();
 
-            arrObj.UsingTempArray(arr);
+//            arrObj.UsingTempArray(arr);
 
-            arrObj.Using2Pointer(arr);
+//            arrObj.Using2Pointer(arr);
 
-            arrObj.UsingBySwapping(arr);
+//            arrObj.UsingBySwapping(arr);
 
-            arrObj.UsingInBuilt(arr);
-        }
-    }
-}
+//            arrObj.UsingInBuilt(arr);
+//        }
+//    }
+//}

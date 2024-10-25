aa6acea83edded7b5e5deb7f7042b08c60f490ad
VinodKumarVeeravalli <vinodveeravalli1712@gmail.com>
2024-10-25 23:54:50 +0530

    uncomment

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            //int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 45, 67 };
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 45, 67 };
 
             //ArrayReverse arrObj = new ArrayReverse();
 
